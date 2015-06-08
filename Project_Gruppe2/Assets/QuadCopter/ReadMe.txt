f76a2be7103af3aff7066df7acee9fb464cd6bac
Sindoora90 <Sindoora@dhcp-10-176-93-189.dynamic.eduroam.mwn.de>
2015-06-08 16:58:38 +0200

    Drohne

---
+++ Project_Gruppe2/Assets/QuadCopter/ReadMe.txt
@@ -0,0 +1,24 @@
+sonaKiS's Factory Model : QUAD COPTER
+(http://sonakis.blogspot.kr/)
+
+
+It is a QuadCopter that can be used for hobby or for military use.
+You can apply the animation to the propeller of four independent.
+Body texture contains 2.
+4 Gimp Templete.
+
+Tris : 21871
+Texture : 1024 px X 1024 px X 5
+
+Body
+ - T_QuadCopter_Body.png = Dark and Red
+ - T_QuadCopter_Body_2.png = Military
+Camera
+ - T_QuadCopter_Carmera
+Frame
+ - T_QuadCopter_Frame
+Propeller
+ - T_QuadCopter_Prop
+
+
+
---
+++ Project_Gruppe2/Assets/Skripts/FlyController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyController : MonoBehaviour {
+
+	public float movementSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+		} 
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+		} 
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.down * movementSpeed * Time.deltaTime*5);
+		} 
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up * movementSpeed * Time.deltaTime*5);
+		} 
+
+	}
+	
+}
