10ddd81f47aa5909a8d6185c0cb2ac55d65d6097
marius <marius.schiele@gmail.com>
2015-06-14 17:55:25 +0200

    - reorganized folder structure - play sound when fire, play sound when cube is hitted

--- Project_Gruppe2/Assets/QuadCopter/ShootDemo.cs
+++ Project_Gruppe2/Assets/QuadCopter/ShootDemo.cs
@@ -1,33 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class ShootDemo : MonoBehaviour {
 	
 	public Rigidbody projectile;
-	
 	public float speed = 100;
-	
-	
+	public AudioClip fireSound;
+
+	private AudioSource fireSource;
 	
 	// Use this for initialization
 	void Start () {
-		
+		fireSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if (Input.GetButtonDown("Fire1"))
-		{
-			Rigidbody instantiatedProjectile = Instantiate(projectile,
-			                                               transform.position,
-			                                               transform.rotation)
-				as Rigidbody;
+		{	
+			// play fire sound when fire button is pressed
+			fireSource.PlayOneShot(fireSound, 1);
+
+			Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
 			
 			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(speed, 0, 0));
-
-			Debug.Log ("Fired!");
-			
 		}
 	}
 
--- Project_Gruppe2/Assets/Skripts/Destroy.cs
+++ Project_Gruppe2/Assets/Skripts/Destroy.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Destroy : MonoBehaviour {
 
+	public AudioClip hitSound;
+	
+	private AudioSource hitSource;
+
 	// Use this for initialization
 	void Start () {
-	
+		hitSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -17,13 +21,15 @@ public class Destroy : MonoBehaviour {
 	{
 		if(col.gameObject.tag == "bullet")
 		{	
+			// play hit sound when cube is hitted by bullet
+			hitSource.PlayOneShot(hitSound, 2);
+
 			//first destroy the bullet
 			Destroy(col.gameObject);
-			Debug.Log ("Bullet destroyed!");
 				
 			// if i am a cube, destroy me
 			if(gameObject.tag == "cube"){
-				Destroy(gameObject);
+//				Destroy(gameObject);
 			}
 
 		}
