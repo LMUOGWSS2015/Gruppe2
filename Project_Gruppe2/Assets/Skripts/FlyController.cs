79523cfc8084e80646f88ad55a11bb92b10a60c0
marius <marius.schiele@gmail.com>
2015-06-14 16:36:34 +0200

    fixed drone movements after drone has been aligned

--- Project_Gruppe2/Assets/Skripts/FlyController.cs
+++ Project_Gruppe2/Assets/Skripts/FlyController.cs
@@ -16,26 +16,26 @@ public class FlyController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.W)) {
 			transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
 		} 
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.S)) {
 			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
 		} 
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.A)) {
-			transform.Rotate(Vector3.down * movementSpeed * Time.deltaTime*5);
+			transform.Rotate(Vector3.left * movementSpeed * Time.deltaTime*5);
 		} 
 		if (Input.GetKey (KeyCode.D)) {
-			transform.Rotate(Vector3.up * movementSpeed * Time.deltaTime*5);
+			transform.Rotate(Vector3.right * movementSpeed * Time.deltaTime*5);
 		}
 		if (Input.GetKey (KeyCode.Q)) {
 //			transform.RotateAround(transform.position, Transform(Vector3.up), Time.deltaTime * 90f);
