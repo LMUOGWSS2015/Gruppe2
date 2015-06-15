171505b111545cb2bbfed407e19bead3524aeea9
Marius Hoggenmüller <marius.hoggenmueller@campus.lmu.de>
2015-06-15 13:02:30 +0200

    shoot particles

--- Project_Gruppe2/Assets/QuadCopter/ShootDemo.cs
+++ Project_Gruppe2/Assets/QuadCopter/ShootDemo.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 public class ShootDemo : MonoBehaviour {
 	
 	public Rigidbody projectile;
 	public float speed = 100;
 	public AudioClip fireSound;
+	public Transform Effect;
+
 
 	private AudioSource fireSource;
 	
@@ -24,6 +26,25 @@ public class ShootDemo : MonoBehaviour {
 			Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
 			
 			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(speed, 0, 0));
+
+			RaycastHit hit;
+			
+			Ray ray = new Ray(transform.position, Vector3.forward);
+
+			
+				if(Physics.Raycast(ray,out hit,100))
+				{		
+					Debug.Log ("RayCast");
+					//string s = hit.point.ToString();
+					//Debug.DrawLine (new Vector3(5,5,5), new Vector3(0,0,0));
+					Debug.Log (hit.point);
+
+					GameObject particleClone = new GameObject();
+					particleClone = Instantiate(Effect, hit.point, Quaternion.LookRotation(hit.normal)) as GameObject;
+					Destroy(particleClone, 2);
+
+
+				}
 		}
 	}
 
---
+++ Project_Gruppe2/Assets/raycastShooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class RaycastShooting : MonoBehaviour
+{
+	public int theDamage;
+	
+	void Update ()
+	{
+		RaycastHit hit;
+		
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2,0));
+		
+		if(Input.GetButtonDown("Fire1"))
+		{
+			if(Physics.Raycast(ray,out hit,100))
+			{
+				
+				Debug.Log ("Working");
+			}
+		}
+	}
+}
