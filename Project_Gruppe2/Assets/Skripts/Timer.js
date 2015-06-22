1ebef2f27d6a6e89ebc7ccc0ec4699228a4af626
aerow90 <kurpiers@gmx.de>
2015-06-22 14:55:12 +0200

    counter beim start

---
+++ Project_Gruppe2/Assets/Skripts/Timer.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var timer : float = 5.0;
+//var counter : Rect = new Rect(Screen.width/2-300, Screen.height/2-150, 100, 100);
+	
+var font : Font;
+var myStyle : GUIStyle; 
+myStyle.normal.textColor = Color.white;
+myStyle.alignment = TextAnchor.MiddleCenter;
+myStyle.fontSize = 200;
+myStyle.font = font;
+//myStyle.font = font;	
+ 	
+function Update() {
+if(timer > 0.0){
+timer -= Time.deltaTime;
+}
+
+
+}
+
+function OnGUI() {
+
+	
+	if(timer>0.0){
+	transform.Translate(Vector3.up/10);
+	GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-100, 100, 100), "" + timer.ToString("0"), myStyle);
+	
+}
+}
