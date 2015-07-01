54537001d314fe7d8a5e0ebce8d4d474359200b7
marius <marius.schiele@gmail.com>
2015-07-01 19:31:02 +0200

    getting old timer back

--- Project_Gruppe2/Assets/Skripts/HUD.cs
+++ Project_Gruppe2/Assets/Skripts/HUD.cs
@@ -6,31 +6,17 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
 	public GUIText text;
-<<<<<<< Updated upstream
 	static int hits = 0;
-=======
-	public float timer = 185.0f;
-	Text guiText;
->>>>>>> Stashed changes
 
 	// Use this for initialization
 	void Start ()
 	{
-		GameObject timeText = GameObject.Find("TimeText");
-		guiText = (Text)timeText.GetComponent (typeof(Text));
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (timer >= 0.0f ) {
-			timer -= Time.deltaTime;
-			if(timer <= 180.0f){
-			guiText.text = " " + Mathf.RoundToInt(timer) + " sec";
-			}
-		}
-
 	
 	}
 
